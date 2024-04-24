d0ff7bf163399ffb44748bd8d68846cda38c14e8
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-24 12:25:47 +0200

    Initial Commit

---
+++ loadingProgress/loadingProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  loadingProgressApp.swift
+//  loadingProgress
+//
+//  Created by Philippe MICHEL on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct loadingProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

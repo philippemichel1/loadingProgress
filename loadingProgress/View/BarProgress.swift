d3b5c09a146592748135f7d91d97245433e3f6e8
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-26 11:33:21 +0200

    Loading project

---
+++ loadingProgress/View/BarProgress.swift
@@ -0,0 +1,8 @@
+//
+//  BarProgress.swift
+//  loadingProgress
+//
+//  Created by Philippe MICHEL on 24/04/2024.
+//
+
+import Foundation
---
+++ loadingProgress/View/CircleProgress.swift
@@ -0,0 +1,18 @@
+//
+//  CircleProgress.swift
+//  loadingProgress
+//
+//  Created by Philippe MICHEL on 24/04/2024.
+//
+
+import SwiftUI
+
+struct CircleProgress: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleProgress()
+}
